85f2906758b5909cf636cf24f33eb5b6564bf4c8
HiiWee <wpdlzhf159@gmail.com>
2022-11-09 14:29:16 +0900

    init: tonton backend project

---
+++ tonton/src/main/java/com/codywiki/tonton/TontonApplication.java
@@ -0,0 +1,13 @@
+package com.codywiki.tonton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TontonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TontonApplication.class, args);
+    }
+
+}
---
+++ tonton/src/test/java/com/codywiki/tonton/TontonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codywiki.tonton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TontonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
