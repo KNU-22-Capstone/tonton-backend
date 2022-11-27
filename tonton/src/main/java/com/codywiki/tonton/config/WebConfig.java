0b0ef4d397433eceddc496cd8ec349556e1f12c9
HiiWee <wpdlzhf159@gmail.com>
2022-11-28 06:27:26 +0900

    feat: cors 8000포트 추가

--- tonton/src/main/java/com/codywiki/tonton/config/WebConfig.java
+++ tonton/src/main/java/com/codywiki/tonton/config/WebConfig.java
@@ -10,8 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("http://localhost:8000")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8000")
                 .allowedMethods("*");
     }
 }
